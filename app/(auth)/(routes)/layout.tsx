98451f59e78e3a0c2aad027f6e5fdb625d56805b
ali zaenal abidin <96227680+alizaenazet@users.noreply.github.com>
2023-08-30 20:11:07 +0700

    feat: add clerk auth page for sign-in and sign-up;

---
+++ app/(auth)/(routes)/layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Layout({
+    children}:{
+    children: React.ReactNode
+    }) {
+  return (
+    <div className='h-full flex items-center justify-center'>{children}</div>
+  )
+}
+
+export default Layout
