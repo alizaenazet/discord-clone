ea6a55a3caf792c64c99539e2974158d317070ce
ali zaenal abidin <96227680+alizaenazet@users.noreply.github.com>
2023-09-04 16:42:31 +0700

    refactor: basic sign-in test

--- tests/e2e/auth.spec.ts
+++ tests/e2e/auth.spec.ts
@@ -4,14 +4,15 @@ import { test, expect } from '@playwright/test';
 
 
 test("redirect to login-page",async ({page}) => {
-    const url = await page.goto('http://localhost:3000').then(() => page.url());
-    // const url = page.url()
-     console.log('url nih boss');
-    console.log(JSON.stringify(url));
-    console.log(url);
     
+    const url = await page.goto('http://localhost:3000').then(() => page.url());
+    // // const url = page.url()
+    //  console.log('url nih boss');
+    // console.log(JSON.stringify(url));
+    // console.log(url);
     // expect to redirect into clerk login page
-     expect(url).toEqual(expect.stringContaining("http://localhost:3000/sign-in?redirect_url="))
+    await expect(page).toHaveURL("/sign-in?redirect_url=http%3A%2F%2Flocalhost%3A3000%2F")
+     expect(page.url()).toEqual(expect.stringContaining("http://localhost:3000/sign-in?redirect_url="))
 })
 
 test("login-page",async ({page}) => {
